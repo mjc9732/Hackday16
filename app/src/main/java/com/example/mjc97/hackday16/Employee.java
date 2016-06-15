4c342e5a49a2690ac0795b7116a818d0caf68b07
Moon Jun Chang <mjc9732@gmail.com>
2016-06-15 14:16:35 -0400

    employee class

---
+++ app/src/main/java/com/example/mjc97/hackday16/Employee.java
@@ -0,0 +1,52 @@
+package com.example.mjc97.hackday16;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by mjc97 on 6/15/2016.
+ */
+public class Employee {
+
+    public  Drawable pic;
+    public  String name;
+    public  String team;
+    public  String department;
+    public  int points;
+
+    public  Employee (Drawable pic, String name, String team, String department, int points) {
+        this.pic = pic;
+        this.name = name;
+        this.team = team;
+        this.department = department;
+        this.points = points;
+    }
+
+    public void addPoints() {
+        points++;
+    }
+
+    public void resetPoimts() {
+        points = 0;
+    }
+
+    public Drawable getPic(){
+        return pic;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public String getTeam(){
+        return team;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+
+}
