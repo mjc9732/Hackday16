7337673d213e7cfabe99f9f341d97ac490032e1a
awilsm <awilsonmyers@gmail.com>
2016-06-16 13:55:29 -0400

    rank button in drawer works

--- app/src/main/java/com/example/mjc97/hackday16/MockProActivity.java
+++ app/src/main/java/com/example/mjc97/hackday16/MockProActivity.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MockProActivity extends AppCompatActivity {
 
     ImageView propic;
     TextView proname;
+    ProgressBar prog1;
+    ProgressBar prog2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +25,18 @@ public class MockProActivity extends AppCompatActivity {
         myToolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(myToolbar);
 
+        prog1 = (ProgressBar) findViewById(R.id.progress);
+        prog2 = (ProgressBar) findViewById(R.id.progress1);
+
+        prog1.setMax(56);
+        prog2.setMax(5);
+        prog1.setProgress(26);
+        prog2.setMax(5);
+
         propic = (ImageView) findViewById(R.id.propic_iv);
         propic.setImageResource(R.drawable.mockface);
-        proname = (TextView) findViewById(R.id.propic_tv);
-        proname.setText("Name: Michael Ball" + "\n" + "Honor received: 17 | Can give: 2");
+        //proname = (TextView) findViewById(R.id.propic_tv);
+        //proname.setText("Name: Michael Ball" + "\n" + "Honor received: 17 | Can give: 2");
 
     }
 }
